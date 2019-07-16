f7a2bbd3d6d7ac1e87582c3d06bc8b5e7d55e370
Surekha <surekhas.ttl@tatamotors.com>
2019-07-16 10:37:41 +0530

    first commit on develop

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+security list-keychains -s ios-build.keychain
+rm ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision 
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "*********************"
+echo "*     Archiving     *"
+echo "*********************"
+xcrun xcodebuild -workspace e-guru.xcworkspace -target e-guru -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+echo "**********************"
+echo "*     Exporting      *"
+echo "**********************"
+xcrun xcodebuild -exportArchive -archivePath $ARCHIVE_NAME.xcarchive -exportPath . -exportOptionsPlist ExportOptions.plist
+./build/Products/e-guru.xcarchive -exportOptionsPlist ./scripts/ExportOptions.plist -exportPath ./build/Products/IPA | xcpretty
